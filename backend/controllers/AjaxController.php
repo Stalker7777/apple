54c079d649a9d285c58679400e7f01896854f25e
Stalker7777 <gabiger7777@mail.ru>
2020-10-21 08:17:58 +0600

    add apple fall

--- backend/controllers/AjaxController.php
+++ backend/controllers/AjaxController.php
@@ -20,10 +20,10 @@ class AjaxController extends Controller
         }
         
         if(count($apple) > 0) {
-            return json_encode(['error' => false, 'apples' => $apple, 'count' => count($apple)]);
+            return json_encode(['is_error' => false, 'apples' => $apple, 'count' => count($apple)]);
         }
         
-        return json_encode(['error' => true, 'message' => 'При получении яблок возникла ошибка!']);
+        return json_encode(['is_error' => true, 'message' => 'При получении яблок возникла ошибка!']);
     }
     
     public function actionGenerateApples()
@@ -31,5 +31,46 @@ class AjaxController extends Controller
         $apples = new Apples();
         
         return json_encode($apples->generate());
+    }
+    
+    public function actionAppleFall()
+    {
+        if (Yii::$app->request->isAjax && Yii::$app->request->isPost) {
+        
+            if(Yii::$app->request->post('id')) {
+                
+                $id = Yii::$app->request->post('id');
+                
+                $apple = Apple::find()->where(['id' => $id])->one();
+                
+                if($apple !== null) {
+    
+                    $apple->status = Apple::STATUS_ON_GROUND;
+                    $apple->fall_date = time();
+                    
+                    if($apple->save()) {
+                        $apple = Apple::find()->where(['id' => $id])->asArray()->one();
+                        return json_encode(['is_error' => false, 'apple' => $apple]);
+                    }
+                    else {
+                        $messages = [];
+                        foreach ($apple->getErrors () as $attribute => $error)
+                        {
+                            foreach ($error as $message)
+                            {
+                                $messages[] = ($attribute.": ".$message);
+                            }
+                        }
+                        return ['is_error' => true, 'message' => implode('; ', $messages)];
+                    }
+                }
+            }
+            else {
+                return json_encode(['is_error' => true, 'message' => 'Ошибка. В запросе отсутствует id яблока!']);
+            }
+            
+        }
+    
+        return json_encode(['is_error' => true, 'message' => 'Ошибка обновления статуса яблока!']);
     }
 }
--- backend/web/js/script.js
+++ backend/web/js/script.js
@@ -13,7 +13,7 @@ function get_apples()
         'success': function(data) {
             var result = JSON.parse(data);
             //console.log(result.error);
-            if(result.error) {
+            if(result.is_error) {
                 alert(result.message);
             }
             else {
@@ -57,49 +57,76 @@ function btn_generate_apples_whow(count)
 
 function create_apples(data)
 {
-    var apples = '';
+    var apples_on_tree = '';
+    var apples_on_ground = '';
 
     console.log(data);
 
     for(var i in data) {
         //console.log(data[i].id);
-        apples += create_apple(data[i]);
+        if(data[i]['status'] == 1) {
+            apples_on_tree += create_apple(data[i]);
+        }
+        else {
+            apples_on_ground += create_apple(data[i]);
+        }
     }
 
-    $('#block_apples_on_tree').html(apples);
+    $('#block_apples_on_tree').html(apples_on_tree);
+    $('#block_apples_on_ground').html(apples_on_ground);
 }
 
 function create_apple(item)
 {
     return '<div id="block_apple_' + item.id + '" class="col-md-4 form-group">'
         + '<input type="hidden" value="' + item.id + '">'
-        + '<input type="hidden" value="' + item.status + '">'
+        + '<input id="apple_status_' + item.id + '" type="hidden" value="' + item.status + '">'
         + '<input type="hidden" value="' + item.color + '">'
         + '<input type="hidden" value="' + item.size + '">'
         + '<img src="' + item.image + '" width="64px" height="64px" alt="apple">'
         + '<button class="btn btn-default" onclick="apple_fall(' + item.id + ')">Упасть</button>'
-        + '<button class="btn btn-primary" onclick="apple_eat()">Съесть</button>'
+        + '<button class="btn btn-primary" onclick="apple_eat(' + item.id + ')">Съесть</button>'
         + '</div>';
 }
 
 function apple_fall(id)
 {
+    var apple_status = $('#apple_status_' + id).val();
+
+    if(apple_status > 1) {
+        alert('Яблоко не на дереве, поэтому упасть уже не может!');
+        return;
+    }
+
     if(confirm('Это яблоко должно упасть?')) {
-
-        // проверяем чтобы статус яблока был на девере, иначе сообщение, что упасть уже не может
-
-        // сделать запрос на сервер
-        // поменять статус яблока
-
-        // закинуть новый статус в яблоко
-
-        apple_fall_update(id);
-
+        $.ajax({
+            url: 'admin/ajax/apple-fall',
+            method: 'post',
+            data: { 'id': id },
+            'error': function() {
+                alert('Ошибка выполнения запроса к серверу!');
+            },
+            'success': function(data) {
+                console.log(data);
+                var result = JSON.parse(data);
+                if(result.is_error) {
+                    alert('При обновлении статуса яблока возникла ошибка!');
+                    console.log(result.message);
+                }
+                else {
+                    apple_fall_update(result.apple);
+                }
+            }
+        });
     }
 }
 
-function apple_fall_update(id)
+function apple_fall_update(data)
 {
+    var id = data['id'];
+
+    $('#apple_status_' + id).val(data['status']);
+
     var apple = $('#block_apple_' + id);
 
     $('#block_apples_on_ground').append(apple);
